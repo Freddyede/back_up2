6fea9bb674c65225cb1b8316fa3171d6e3c54a52
Franck <patouillardfranck3@gmail.com>
2020-02-13 11:43:41 +0100

    edit README.md

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class AccueilController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="accueil")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('accueil/index.html.twig', [
+            'controller_name' => 'AccueilController',
         ]);
     }
 }
